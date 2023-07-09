da9245995ede40e1fc35f58f073b39fdc447b17d
Sourav0809 <spathak7431@gmail.com>
2023-07-09 19:32:36 +0530

    already have the header make some changes

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -5,9 +5,8 @@ const Header = () => {
     <header className="header">
       <h2 className="heading">Biriyani House</h2>
       <div className="header-icons">
-        <div className="cart-icons">
-          <i className="bx bx-cart-alt">Cart</i>
-        </div>
+        <i className="bx bx-cart-alt">Cart</i>
+        <h2 className="cart-quantity">0</h2>
         <i className="bx bx-user">SignUp</i>
       </div>
     </header>
